fb9202904f64f9dadc4c0861b64e5acef7fc01c0
KouKariya <KouKariya@users.noreply.github.com>
2021-06-25 11:42:03 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# High School Medium
+# RPG game about a high schooler discovering spiritual powers and fighting monsters
+# Written in Python 2 + PyGame
+# By Jesse Gallarzo
+import pygame
+
+#Game variables
+
+#Start the game
+def startGame():
+    print("Filler statement.")
+
+#TODO change interpreter to python 2 to make PyGame work
+def main():
+    gameStart = False
+    while gameStart is False:
+        
+        print("Test")
+        gameStart = True
+        
+main()
